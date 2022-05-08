a0d06bec0a6612b6c8ac0cb6f2e09a394a7b3dd3
minskkoasdsadgfsdkl <86870992+minskkoasdsadgfsdkl@users.noreply.github.com>
2022-05-08 12:00:31 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.Node_ENV === "production" ? "/baemin_proj/" : "/",
 });
