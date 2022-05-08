6894e219232ee22212a4c9eb31945fac1deb82ee
minskkoasdsadgfsdkl <86870992+minskkoasdsadgfsdkl@users.noreply.github.com>
2022-05-08 11:36:48 +0900

    "첫 업데이트

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+import BootstrapVue3 from "bootstrap-vue-3";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+
+createApp(App).use(router).use(BootstrapVue3).mount("#app");
